96cb6900611498c1fe91774524ef1b4d481e2319
Lars Hedlund <lkhedlund@gmail.com>
2022-08-05 13:56:57 -0600

    add(thinkific): aws login alias

--- zsh/thinkific.zsh
+++ zsh/thinkific.zsh
@@ -1,4 +1,6 @@
 # Thinkific-specific Setup
 ssh-add --apple-load-keychain &> /dev/null
+alias awslogin="aws --profile think-dev sso login"
+
 export THINKIFICPATH=/Users/lars/workspace
-export DEV_PACKAGES="zsh vim"
+export DEV_PACKAGES="zsh vim imagemagick"
