83eda80e32b3662cc01ee05edd605a9a719e0245
Lars Hedlund <lkhedlund@gmail.com>
2022-10-17 16:58:17 -0600

    feat(alias): move rails aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,3 +6,7 @@
 alias gc="git commit -m"
 alias gco="git checkout"
 alias clean-old-branches="git branch --merged | egrep -v \"(^\*|master|dev)\" | xargs git branch -d"
+alias rails-up="bin/rails s"
+alias rails-reset="bin/spring stop && bin/rails s"
+alias bx="bundle exec"
+alias bxr="bundle exec rspec"
--- zsh/thinkific.zsh
+++ zsh/thinkific.zsh
@@ -1,9 +1,6 @@
 # Thinkific-specific Setup
 ssh-add --apple-load-keychain &> /dev/null
 alias awslogin="aws --profile think-dev sso login"
-alias rails-up="bin/rails s"
-alias bx="bundle exec"
-alias bxr="bundle exec rspec"
 
 export THINKIFICPATH=/Users/lars/workspace
 export DEV_PACKAGES="zsh vim imagemagick"
