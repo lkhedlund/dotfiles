f905c0e54017b9ad3c12aa2ccb00135b134596af
Lars Hedlund <lkhedlund@gmail.com>
2022-11-04 16:56:28 -0600

    fix(thinkific): move to container commands

--- zsh/dev-container.zsh
+++ zsh/dev-container.zsh
@@ -1,3 +1,19 @@
 # ╔═══════════════════════╗
 # ║  CONTAINER CONFIG     ║
 # ╚═══════════════════════╝
+func engine_rspec() {
+  # Provide an engine rspec path, i.e. `engines/communities/type.spec`
+  if [[ $1 =~ (engines\/[A-Za-z]+)\/(.*) ]]
+  then
+    bundle exec rspec ${BASH_REMATCH[2]}
+  else
+    bin/rails rspec $1
+  fi
+}
+
+func create_pr() {
+  # Defaults to staging, but any branch can be passed in
+  base_branch=${1:-staging}
+  current_branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+  echo gh pr create -B $base_branch -t $current_branch
+}
