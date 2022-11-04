d68affa114ef464c591d1f2d026dabcd84efa87c
Lars Hedlund <lkhedlund@gmail.com>
2022-11-04 17:11:29 -0600

    fix(container): small rspec adjustment

--- zsh/dev-container.zsh
+++ zsh/dev-container.zsh
@@ -1,19 +1,18 @@
 # ╔═══════════════════════╗
 # ║  CONTAINER CONFIG     ║
 # ╚═══════════════════════╝
-func engine_rspec() {
+func run_rspec() {
   # Provide an engine rspec path, i.e. `engines/communities/type.spec`
   if [[ $1 =~ (engines\/[A-Za-z]+)\/(.*) ]]
   then
-    bundle exec rspec ${BASH_REMATCH[2]}
+    bundle exec rspec ${match[2]}
   else
     bin/rails rspec $1
   fi
 }
-
 func create_pr() {
   # Defaults to staging, but any branch can be passed in
   base_branch=${1:-staging}
   current_branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
-  echo gh pr create -B $base_branch -t $current_branch
+  gh pr create -B $base_branch -t $current_branch
 }
