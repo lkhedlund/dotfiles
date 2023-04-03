92b0bafdca3f16985f834e3aa54227e249c7ceab
Lars Hedlund <lkhedlund@gmail.com>
2023-04-03 16:55:21 -0600

    chore(aliases): move aliases out

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,3 +6,9 @@
 alias gc="git commit -m"
 alias gco="git checkout"
 alias gclean="git branch --merged | egrep -v \"(^\*|master|dev)\" | xargs git branch -d"
+# Rails
+alias rails-up="bin/rails s"
+alias rails-reset="bin/spring stop && bin/rails s"
+alias bx="bundle exec"
+alias bxr="bundle exec rspec"
+alias shoryuken-up='bundle exec shoryuken -R -C config/shoryuken.yml'
--- zsh/thinkific.zsh
+++ zsh/thinkific.zsh
@@ -2,12 +2,6 @@
 ssh-add --apple-load-keychain &> /dev/null
 
 alias awslogin="aws --profile think-dev sso login"
-alias rails-up="bin/rails s"
-alias rails-reset="bin/spring stop && bin/rails s"
-alias bx="bundle exec"
-alias bxr="bundle exec rspec"
-alias sidekiq-up='redis-cli -h redis flushall && bundle exec sidekiq'
-alias shoryuken-up='bundle exec shoryuken -R -C config/shoryuken.yml'
 
 export THINKIFICPATH=/Users/lars/workspace
 export DEV_PACKAGES="zsh vim imagemagick"
