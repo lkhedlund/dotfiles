a3beebcafad017de451d090085ebc165c77491ab
Lars Hedlund <lkhedlund@gmail.com>
2024-02-05 09:32:16 -0700

    chore(git): changed to signed

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,7 +3,7 @@
 # ╚═══════════════════════╝
 
 # Version control
-alias gc="git commit -m"
+alias gc="git commit -S -m"
 alias gco="git checkout"
 alias gclean="git branch --merged | egrep -v \"(^\*|master|dev)\" | xargs git branch -d"
 # Rails
